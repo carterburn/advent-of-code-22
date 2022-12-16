965fd960c27bedf57c872303f5425a88484f789c
Carter Burn <carter.burn3@gmail.com>
2022-12-15 21:10:12 -0500

    day10 part1 done

---
+++ day10/prob1.py
@@ -0,0 +1,38 @@
+import sys
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    fname = 'test.txt'
+with open(fname,'r') as fi:
+    lines = list(map(str.strip, fi.readlines()))
+
+measurements = []
+
+def signal_strength(cyc, reg):
+    if cyc == 20 or (((cyc-20) % 40) == 0):
+        # take the measurement
+        measurements.append(cyc * reg)
+
+# difficulty here is measuring DURING cycles
+# addx will finish execution after 2 cycles
+X = 1
+cycle = 1
+for inst in lines:
+    inst = inst.split(' ')
+    # start of cycle 'cycle'
+    # measure X DURING this cycle
+    signal_strength(cycle, X)
+    if inst[0] == 'noop':
+        # increase cycle by 1, X is unchanged
+        cycle += 1
+    elif inst[0] == 'addx':
+        # finish cycle 'cycle'
+        cycle += 1
+        # start new cycle, measure X DURING this cycle
+        signal_strength(cycle, X)
+        # end cycle
+        cycle += 1
+        # finish execution of addx instruction
+        X += int(inst[1])
+        
+print('answer',sum(measurements))
