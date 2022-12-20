708345eabeec896690fa85a7e811a9ce60bfb57d
Carter Burn <carter.burn3@gmail.com>
2022-12-19 21:23:57 -0500

    day13 prob1 done, had to reorganize and get help on the ocmpare function

--- day13/prob1.py
+++ day13/prob1.py
@@ -11,69 +11,24 @@ for i in range(0,len(lines),3):
     pairs.append((eval(lines[i]), eval(lines[i+1])))
 
 def compare(left, right):
-    # check lengths
-    if len(left) == 0:
-        # left ran out
-        return True
-    if len(right) == 0:
-        # right ran out
-        return False
-
-    # check type mismatch of first value
-    l = left[0]
-    r = right[0]
-
-    if type(l) != type(r):
-        if type(l) == int:
-            l = [l]
+    l = left if isinstance(left, list) else [left]
+    r = right if isinstance(right, list) else [right]
+    for l2, r2 in zip(l,r):
+        if isinstance(l2, list) or isinstance(r2, list):
+            res = compare(l2, r2)
         else:
-            r = [r]
-
-    # check if out of order condition exists if l and r are ints
-    if type(l) == int:
-        if r > l:
-            return False
-
-    # if they're lists, drill down on the list comparing
-    if not compare(l, r):
-        return False
-
-    # continue with list
-    return compare(left[1:], right[1:])
+            res = r2 - l2
+        if res != 0:
+            return res
+    return len(r) - len(l)
 
 correct = []
 for i in range(len(pairs)):
     res = compare(pairs[i][0], pairs[i][1])
-    if res:
+    print(res)
+    if res > 0:
         # not 0 indexed for the answer
         correct.append(i+1)
 
+print(correct)
 print('answer:', sum(correct))
-"""
-def compare(left, right):
-    # check lenghts
-    if len(left) == 0:
-        # left side ran out
-        return True
-    if len(right) == 0:
-        # right side ran out
-        return False
-
-    l = left[0]
-    r = right[0]
-
-    # mismatch?
-    if type(l) != type(r):
-        if type(l) == int:
-            # convert l to list
-            l = [l]
-        else:
-            r = [r]
-
-    if type(l) == list:
-        # drill down another comparison
-        if not compare(l,r):
-            return False
-
-
-"""
